b7f5aaabd27922a790ec5bdbaad1970142d311a9
Phloser <phloser@icloud.com>
2021-01-27 18:12:03 +0300

    task1

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -4,9 +4,30 @@ namespace HomeWork_2
 {
     class Program
     {
-        static void Main(string[] args)
+
+         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите 3 числа");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int min ;
+
+            if (a < b)
+                 {
+                    if (b < c)
+                        { min = b; } 
+                   else { min = c; }
+                 }
+            else { 
+                    if (a < c) 
+                         { min = a; } 
+                    else { min = c; } 
+                 }
+            Console.WriteLine($"Самое маленькое число {min}");
+
         }
+
+        
     }
 }
