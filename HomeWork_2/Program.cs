9ea3ce17fc5e0f55ab3edc759215dd1398cab42b
Phloser <phloser@icloud.com>
2021-01-28 18:32:11 +0300

    2,5 task

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -4,8 +4,7 @@ namespace HomeWork_2
 {
     class Program
     {
-
-         static void Main()
+        static void Task1()
         {
             Console.WriteLine("Введите 3 числа");
             int a = int.Parse(Console.ReadLine());
@@ -25,6 +24,57 @@ namespace HomeWork_2
                     else { min = c; } 
                  }
             Console.WriteLine($"Самое маленькое число {min}");
+            Console.ReadKey();
+            Console.Clear();
+        } // Написать метод, возвращающий минимальное из трех чисел
+        static void Task2()
+        {
+            Console.WriteLine("Введите число");
+            string a = Console.ReadLine();
+            int count = 0;
+            foreach (char symbol in a)
+            {
+                count++;
+            }
+            Console.WriteLine($"в числе {count} знаков");
+            Console.ReadKey();
+            Console.Clear();
+        } // Написать метод подсчета количества цифр числа
+        static void Task3()
+        {
+            int y = 0; int x = 1;
+            Console.WriteLine("ВВодите числа (окончание ввода 0)");
+            while (x > 0; x < 0)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x % 2 == 0) { }
+                else { y = y + x; }
+            }
+        } // С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел
+
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Введите номер задания (0 - выход):");
+                var index = int.Parse(Console.ReadLine());
+
+                switch (index)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        Task1();
+                        break;
+                    case 2:
+                        Task2();
+                        break;
+                    case 3:
+                        Task3();
+                        break;
+                }
+            }
 
         }
 
